693177e9d8ca326ba192d637feebc8b70fc88d2c
Shan1024 <gambit1024@gmail.com>
2015-06-10 18:39:54 +0530

    Initial commit

---
+++ Algorithms/Combinatorics/MinimumDraws.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+
+            int n = scanner.nextInt();
+            System.out.println(n + 1);
+
+        }
+    }
+}
