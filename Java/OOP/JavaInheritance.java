72531fec2204e60f0a2653718df724063b38470b
Shan1024 <gambit1024@gmail.com>
2015-06-13 01:17:19 +0530

    Initial commit

---
+++ Java/OOP/JavaInheritance.java
@@ -0,0 +1,10 @@
+ class Arithmetic{
+    public  int add(int a, int b){
+        return 0;
+    }
+}
+class Adder extends Arithmetic{
+    public int add(int a, int b){
+        return a+b;
+    }
+}
